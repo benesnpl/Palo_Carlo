d929510762b6c55a11bad0a645872663823a0b99
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-06-29 14:12:18 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -12,6 +12,6 @@ provider "azurerm" {
   features {}
   subscription_id = "643419c7-11b4-43c2-9d6a-ee115f86627f"
   client_id = "edb0ff32-e89f-40cb-88dc-a02959ae2a5a"
-  client_secret = "3d859d8f-f4c9-4ac0-b236-de4020535d08"
+  client_secret = "qtX8Q~DQ1_WV-drIqfXN8pJrD1dmh_xEhB6J5bQ1"
   tenant_id = "cf668d98-df58-45c9-86fb-b72c2761364b"
 }
